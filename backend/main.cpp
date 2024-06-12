51d0d45ef79cf8d737c9f2364955971e52714d2f
Kaja Prestnes Lind <115428762+OrganicCarrot4095@users.noreply.github.com>
2024-06-12 19:27:04 +0200

    Initial commit

---
+++ backend/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, WeatherApp!" << std::endl;
+    return 0;
+}
---
+++ build/CMakeFiles/WeatherApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WeatherApp.dir/backend/main.cpp.o"
+  "CMakeFiles/WeatherApp.dir/backend/main.cpp.o.d"
+  "WeatherApp"
+  "WeatherApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WeatherApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
